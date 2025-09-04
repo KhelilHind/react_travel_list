8a8713870b26fc88bdf2d472d59eaa074424116a
khelilHind <hindkhelil34@gmail.com>
2025-09-04 14:04:22 +0100

    Building a form and handling submission.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 
 function Form() {
+	function handleSubmition(e) {
+		e.preventDefault();
+		console.log(e);
+	}
 	return (
-		<div className="add-form">
-			<h3>What do you need for your trip?</h3>
-		</div>
+		<form className="add-form" onSubmit={handleSubmition}>
+			<h3>What do you need for your trip 🤩?</h3>
+			<select>
+				{Array.from({ length: 20 }, (_, i) => i + 1).map((num) => {
+					return (
+						<option value={num} key={num}>
+							{num}
+						</option>
+					);
+				})}
+			</select>
+			<input type="text" placeholder="Enter You Item..." />
+			<button>Add</button>
+		</form>
 	);
 }
 
