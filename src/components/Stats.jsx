62244cd10897f70b15ec206ca139637b4e38d7b8
KhelilHind <hindkhelil34@gmail.com>
2026-09-25 19:14:00 +0100

    Update Stats component to display the number of items dynamically

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
-function Stats() {
-	return (
-		<footer className="stats">
-			<em>You have X items on you list, and you already packed X (X%).</em>
-		</footer>
-	);
+function Stats({ items }) {
+  const numItems = items.length;
+  return (
+    <footer className="stats">
+      <em>
+        You have {numItems} items on you list, and you already packed X (X%).
+      </em>
+    </footer>
+  );
 }
 
 export default Stats;
